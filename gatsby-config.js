0927235e726743fd3207091ee9cdb3c92dea5fdf
Darren Li <beckzayi@126.com>
2021-05-11 14:27:40 +1000

    feat: add mdx, gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-mdx',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/src/pages/`
+      }
+    },
     {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
